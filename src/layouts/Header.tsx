b650498b045ac835fec0665e06f81bdf445a2d93
Tomas Sramek <tom@wptom.com>
2023-06-14 12:48:01 +0200

    Path to avatar fix

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -129,7 +129,7 @@ const Header = () => {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                <Avatar alt="Remy Sharp" src="/vite.svg" />
                             </IconButton>
                         </Tooltip>
                         <Menu
